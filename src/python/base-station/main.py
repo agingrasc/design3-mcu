1b504e4b40fdb55a8a657e8e622119129ed82bc3
Nathaniel Simard <nathaniel@localhost.localdomain>
2017-01-31 22:33:58 -0500

    serve index.html

--- src/python/base-station/main.py
+++ src/python/base-station/main.py
@@ -1,16 +1,20 @@
+import json
 import requests
 from flask import Flask, current_app
 app = Flask(__name__)
+
+URL = "http://localhost"
+PORT = ":5000/"
 
 @app.route('/')
 def hello_world():
     return current_app.send_static_file('index.html')
 
-URL = "http://localhost"
-PORT = ":5000/"
-
-#response = requests.post(URL + PORT + "goto-position")
-#print(response.status_code)
-#print(response.content)
+@app.route("/goto-position")
+def goto_position():
+    #response = requests.post(URL + PORT + "goto-position")
+    #print(response.status_code)
+    #print(response.content)
+    return json.dumps("Goto position"), 200
 
 app.run()
