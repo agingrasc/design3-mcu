a121f3b5def729eb4377ffdf730092dde6add2d3
Nathaniel Simard <nathaniel@localhost.localdomain>
2017-02-06 11:05:31 -0500

    push junk

--- src/python/base-station/main.py
+++ src/python/base-station/main.py
@@ -1,6 +1,6 @@
 import json
-import requests
-from flask import Flask, current_app
+import requests as req
+from flask import Flask, request, current_app
 app = Flask(__name__)
 
 ROBOT_API_URL = "http://localhost:5000/"
@@ -17,11 +17,17 @@ def static_proxy(path):
     return app.send_static_file(path)
 
 
-@app.route("/go-to-position")
+@app.route("/go-to-position/", methods=['POST'])
 def goto_position():
-    response = requests.post(ROBOT_API_URL + "go-to-position")
+    pos_x = request.form['x']
+    pos_y = request.form['y']
+    print(pos_x)
+    print(pos_y)
+    response = req.post(ROBOT_API_URL + "go-to-position")
     print(response.status_code)
     print(response.content)
+    print(response.content[0])
+    print(response.content[1])
     return json.dumps("succeed"), 200
 
 app.run(port=PORT)
