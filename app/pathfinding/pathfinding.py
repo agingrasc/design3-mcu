cc5272519ea108e8419f28264e7ec0db56bc6052
nathaniel <nathaniel.simard.1@ulaval.ca>
2017-02-20 14:10:17 -0500

    ça marche :D

--- app/pathfinding/pathfinding.py
+++ app/pathfinding/pathfinding.py
@@ -1,22 +1,13 @@
-
-
-
-class PathFinding:
-    def __init__(self):
-        pass
-
-    def find(self, grid, begin_position, end_position):
-        initialise_weight(grid, end_position)
-        path = []
-        current_neighbor = begin_position
-        while current_neighbor.weight > 0:
-            print("x: " + str(current_neighbor.pos_x) + " y : " + str(current_neighbor.pos_y))
-            neighbors = grid.neighbors(current_neighbor)
-            new_neighbors = removed_already_visited_neighbors(neighbors, path)
-            current_neighbor = find_minimum(new_neighbors)
-            current_neighbor.set_path()
-            path.append(current_neighbor)
-        return path
+def find(grid, begin_position, end_position):
+    path = []
+    current_neighbor = begin_position
+    while current_neighbor.weight > 0:
+        neighbors = grid.neighbors(current_neighbor)
+        new_neighbors = removed_already_visited_neighbors(neighbors, path)
+        current_neighbor = find_minimum(new_neighbors)
+        current_neighbor.set_path()
+        path.append(current_neighbor)
+    return path
 
 
 def find_minimum(neighbors):
@@ -26,6 +17,7 @@ def find_minimum(neighbors):
             current_neighbor = neighbor
     return current_neighbor
 
+
 def removed_already_visited_neighbors(neighbors, path):
     new_neighbors = []
     for neighbor in neighbors:
@@ -33,15 +25,10 @@ def removed_already_visited_neighbors(neighbors, path):
             new_neighbors.append(neighbor)
     return new_neighbors
 
-def compare_coord(coord1, coord2):
-    if coord1.pos_x != coord2.pos_x:
-        return False
-    if coord2.pos_y != coord2.pos_y:
-        return False
 
-def initialise_weight(grid, begin_position):
-    neighbors = grid.neighbors(begin_position)
-    next_weight = begin_position.weight +1
+def initialise_weight(grid, begin_position, increment_size):
+    neighbors = set(grid.neighbors(begin_position))
+    next_weight = begin_position.weight + increment_size
     next_neighbors = []
     while len(neighbors) > 0:
         for neighbor in neighbors:
@@ -50,7 +37,51 @@ def initialise_weight(grid, begin_position):
                 next_neighbors.append(neighbor)
         neighbors = []
         for next_neighbor in next_neighbors:
-            new_neighbors = [x for x in grid.neighbors(next_neighbor) if x.weight == -1]
+            new_neighbors = [
+                x for x in grid.neighbors(next_neighbor) if x.weight == -1
+            ]
             neighbors += new_neighbors
-        next_weight += 1
+        next_weight += increment_size
         neighbors = set(neighbors)
+
+def ajust_left_obstacle(grid, position, radius, width, length):
+    x_position = position.pos_x + radius
+    y_min_position = position.pos_y - radius
+    y_max_position = position.pos_y + radius
+    for x in range(x_position):
+        increment = x_position - x
+        for y in range(y_max_position, y_max_position + x_position - x):
+            if y >= 0 and x >= 0 and x < width and y <length:
+                node = grid.game_board[x][y]
+                node.set_weight(node.weight + increment)
+                increment -= 1
+        increment = x_position - x
+        for y in range(y_min_position, y_min_position - x_position + x, -1):
+            if y >= 0 and x >= 0 and x < width and y <length:
+                node = grid.game_board[x][y]
+                ajust_one_node(node, increment)
+                increment -= 1
+
+def ajust_right_obstacle(grid, position, radius, width, length):
+    x_position = position.pos_x - radius-1
+    y_min_position = position.pos_y - radius
+    y_max_position = position.pos_y + radius
+    for x in range(width, x_position, -1):
+        increment = x - x_position
+        for y in range(y_max_position, y_max_position + x - x_position):
+            print(y)
+            if y >= 0 and x >= 0 and x < width and y <length:
+                node = grid.game_board[x][y]
+                ajust_one_node(node, increment)
+                increment -= 1
+        increment = x - x_position
+        for y in range(y_min_position, y_min_position - x + x_position, -1):
+            if y >= 0 and x >= 0 and x < width and y <length:
+                node = grid.game_board[x][y]
+                ajust_one_node(node, increment)
+                increment -= 1
+
+def ajust_one_node(node, ajustement):
+    if node.weight != 0:
+        node.set_weight(node.weight + ajustement)
+
--- app/pathfinding/test_pathfinding.py
+++ app/pathfinding/test_pathfinding.py
@@ -1,74 +1,59 @@
-import unittest
-from unittest.mock import Mock
-from unittest.mock import MagicMock
-from . import pathfinding
+from unittest import TestCase
 from app.gameboard.gameboard import GameBoard
 from app.gameboard.gameboard import Coordinate
 from app.gameboard.gameboard import ObstacleBuilder
 from app.gameboard.gameboard import ObstacleValueObject
 from app.gameboard.gameboard import Tag
 from app.pathfinding.graph import Grid
+from . import pathfinding
 
-WIDTH = 4
-LENGHT = 4
-
-
-class PathFindingTest(unittest.TestCase):
-
+class PathFindingTest(TestCase):
     def test_already_visited_neighbors(self):
-        coord1 = Coordinate(1,0)
-        coord2 = Coordinate(2,0)
-        coord3 = Coordinate(3,0)
-        coord4 = Coordinate(4,0)
+        coord1 = Coordinate(1, 0)
+        coord2 = Coordinate(2, 0)
+        coord3 = Coordinate(3, 0)
+        coord4 = Coordinate(4, 0)
         array1 = []
         array1.append(coord1)
         array1.append(coord2)
         array1.append(coord3)
         array1.append(coord4)
-        
+
         array2 = []
         array2.append(coord1)
         array2.append(coord2)
-        
+
         array3 = pathfinding.removed_already_visited_neighbors(array1, array2)
         self.assertEqual(array3[0], coord3)
         self.assertEqual(array3[1], coord4)
 
-
-    def test_integration1(self):
+    def test_initialise_weight_one_obstacle(self):
         obstacle_builder = ObstacleBuilder()
-        obstacle = ObstacleValueObject(
-            pos_x=4,
-            pos_y=9,
-            radius=1,
-            tag='')
+        obstacle = ObstacleValueObject(pos_x=4, pos_y=9, radius=1, tag='')
         obstacle_builder.add_obtacle(obstacle)
         board = GameBoard(13, 13, obstacle_builder)
 
         grid = Grid(board)
         board.print_game_board()
-        begin_position = grid.game_board[1][1]
-        begin_position.set_weight(0)
-        
-        pathfinding.initialise_weight(grid, begin_position)
+        end_position = grid.game_board[1][1]
+        end_position.set_weight(0)
+
+        pathfinding.initialise_weight(grid, end_position, 1)
         board.print_game_board_weight()
 
-
-
-    def test_integration2(self):
+    def test_initialise_weight_no_obstacle(self):
         obstacle_builder = ObstacleBuilder()
         board = GameBoard(6, 6, obstacle_builder)
 
         grid = Grid(board)
         board.print_game_board()
-        begin_position = grid.game_board[2][2]
-        begin_position.set_weight(0)
+        end_position = grid.game_board[2][2]
+        end_position.set_weight(0)
 
-        pathfinding.initialise_weight(grid, begin_position)
+        pathfinding.initialise_weight(grid, end_position, 1)
         board.print_game_board_weight()
 
-
-    def test_integration3(self):
+    def test_find_no_obstacle(self):
         obstacle_builder = ObstacleBuilder()
         board = GameBoard(6, 6, obstacle_builder)
 
@@ -78,25 +63,53 @@ class PathFindingTest(unittest.TestCase):
         begin_position = grid.game_board[5][5]
         end_position.set_weight(0)
 
-        pathfinder = pathfinding.PathFinding()
-        path =pathfinder.find(grid, begin_position, end_position)
+        pathfinding.initialise_weight(grid, end_position, 5)
+        pathfinding.find(grid, begin_position, end_position)
         board.print_game_board_weight()
         board.print_game_board()
 
-
-    def test_integration4(self):
+    def test_find_ajust_obstacle(self):
         obstacle_builder = ObstacleBuilder()
         obstacle = ObstacleValueObject(
-            pos_x=14,
-            pos_y=19,
-            radius=3,
-            tag=Tag.CANT_PASS_LEFT)
+            pos_x=14, pos_y=19, radius=3, tag=Tag.CANT_PASS_LEFT)
+        obstacle_builder.add_obtacle(obstacle)
+        board = GameBoard(30, 55, obstacle_builder)
+
+        grid = Grid(board)
+        end_position = grid.game_board[8][50]
+        begin_position = grid.game_board[2][2]
+        end_position.set_weight(0)
+
+        pathfinding.initialise_weight(grid, end_position, 55)
+        pathfinding.ajust_left_obstacle(grid, Coordinate(14,19), 3,30 ,55)
+        pathfinding.find(grid, begin_position, end_position)
+        board.print_game_board()
+        board.print_game_board_weight()
+
+    def test_find_1_obstacle(self):
+        obstacle_builder = ObstacleBuilder()
+        obstacle = ObstacleValueObject(
+            pos_x=14, pos_y=19, radius=3, tag=Tag.CANT_PASS_LEFT)
+        obstacle_builder.add_obtacle(obstacle)
+        board = GameBoard(30, 55, obstacle_builder)
+
+        grid = Grid(board)
+        end_position = grid.game_board[8][50]
+        begin_position = grid.game_board[2][2]
+        end_position.set_weight(0)
+
+        pathfinding.initialise_weight(grid, end_position, 55)
+        pathfinding.find(grid, begin_position, end_position)
+        board.print_game_board()
+        board.print_game_board_weight()
+
+    def test_find_2_obstacle(self):
+        obstacle_builder = ObstacleBuilder()
+        obstacle = ObstacleValueObject(
+            pos_x=14, pos_y=19, radius=3, tag=Tag.CANT_PASS_LEFT)
         obstacle_builder.add_obtacle(obstacle)
         obstacle2 = ObstacleValueObject(
-            pos_x=5,
-            pos_y=39,
-            radius=3,
-            tag=Tag.CANT_PASS_RIGHT)
+            pos_x=5, pos_y=39, radius=3, tag=Tag.CANT_PASS_RIGHT)
         obstacle_builder.add_obtacle(obstacle2)
         board = GameBoard(30, 55, obstacle_builder)
 
@@ -105,7 +118,29 @@ class PathFindingTest(unittest.TestCase):
         begin_position = grid.game_board[2][2]
         end_position.set_weight(0)
 
-        pathfinder = pathfinding.PathFinding()
-        pathfinder.find(grid, begin_position, end_position)
+        pathfinding.initialise_weight(grid, end_position, 55)
+        pathfinding.find(grid, begin_position, end_position)
         board.print_game_board()
+        board.print_game_board_weight()
 
+    def test_find_ajust_2_obstacle(self):
+        obstacle_builder = ObstacleBuilder()
+        obstacle = ObstacleValueObject(
+            pos_x=14, pos_y=19, radius=3, tag=Tag.CANT_PASS_LEFT)
+        obstacle_builder.add_obtacle(obstacle)
+        obstacle2 = ObstacleValueObject(
+            pos_x=5, pos_y=39, radius=3, tag=Tag.CANT_PASS_RIGHT)
+        obstacle_builder.add_obtacle(obstacle2)
+        board = GameBoard(30, 55, obstacle_builder)
+
+        grid = Grid(board)
+        end_position = grid.game_board[8][50]
+        begin_position = grid.game_board[2][2]
+        end_position.set_weight(0)
+
+        pathfinding.initialise_weight(grid, end_position, 55)
+        pathfinding.ajust_left_obstacle(grid, Coordinate(14,19), 3,30 ,55)
+        pathfinding.ajust_right_obstacle(grid, Coordinate(5,39), 3,30 ,55)
+        pathfinding.find(grid, begin_position, end_position)
+        board.print_game_board()
+        board.print_game_board_weight()
