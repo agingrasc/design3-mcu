1ee27ec6d9a719d6dec13cf2169b8ab99f7af6d5
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2017-02-20 13:06:11 -0500

    ajout code identification

--- tests/identification.py
+++ tests/identification.py
@@ -1,3 +1,4 @@
+import csv
 import time
 import sys
 
@@ -9,20 +10,27 @@ from util import *
 IDENTIFICATION_TIME = 0.5
 STEP = 0.030
 COMMANDS = [20, 30, 40, 60, 70, 80]
+COMPANION_COMMAND = 40
 
 
-motors = {"frontx": protocol.Motors.FRONT_X,
-          "rearx": protocol.Motors.REAR_X,
-          "fronty": protocol.Motors.FRONT_Y,
-          "reary": protocol.Motors.REAR_Y}
+motors = {"frontx": (protocol.Motors.FRONT_X, protocol.Motors.REAR_X),
+          "rearx": (protocol.Motors.REAR_X, protocol.Motors.FRONT_X),
+          "fronty": (protocol.Motors.FRONT_Y, protocol.Motors.REAR_Y),
+          "reary": (protocol.Motors.REAR_Y, protocol.Motors.FRONT_Y)}
 
 ser = serial.Serial("/dev/ttySTM32")
 
 
-def direction_test(motor_id: protocol.Motors, consigne, retroaction):
+def direction_test(motors_id: tuple, consigne, retroaction):
+    main_id, companion_id = motors_id
+    dir_sign = '+'
     for direction in protocol.MotorsDirection:
+        if direction == protocol.MotorsDirection.FORWARD:
+            dir_sign = '+'
+        else:
+            dir_sign = '-'
         for cmd in COMMANDS:
-            ser.write(protocol.generate_manual_speed_command(motor_id, cmd, direction))
+            set_consigne(cmd, companion_id, direction, main_id)
             begin = time.time()
             id_time = time.time() - begin
             last_tick = time.time()
@@ -31,25 +39,41 @@ def direction_test(motor_id: protocol.Motors, consigne, retroaction):
                 delta_t = now - last_tick
                 if delta_t > STEP:
                     last_tick = now
-                    speed = read_encoder(motor_id)
-                    consigne.append(cmd)
+                    speed = read_encoder(main_id)
+                    if dir_sign == '+':
+                        consigne.append(cmd)
+                    else:
+                        consigne.append(-cmd)
                     retroaction.append(speed)
                     print("({}) val: {}".format(delta_t, speed))
                 id_time = now - begin
     print("{}\n{}".format(consigne, retroaction))
 
 
-def main(motor_id: protocol.Motors):
+def set_consigne(cmd, companion_id, direction, main_id):
+    ser.write(protocol.generate_manual_speed_command(main_id, cmd, direction))
+    ser.read(2)
+    ser.write(protocol.generate_manual_speed_command(companion_id, COMPANION_COMMAND, direction))
+    ser.read(2)
+
+
+def main(motors_id: tuple):
     ser.write(protocol.generate_toggle_pid())
     ser.read()
     consigne = []
     retroaction = []
-    direction_test(motor_id, consigne, retroaction)
-
+    direction_test(motors_id, consigne, retroaction)
     ser.write(protocol.generate_toggle_pid())
-    print("finish")
+
+    with open(fname+'.csv', 'w') as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow(consigne)
+        writer.writerow(retroaction)
+
+    print("Finish")
 
 
 if __name__ == "__main__":
+    fname = sys.argv[1]
     main(motors[sys.argv[1]])
 
