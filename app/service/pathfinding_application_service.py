d10e753b57af49a5244447cf279a7c6c0d0eb648
nathaniel <nathaniel.simard.1@ulaval.ca>
2017-02-21 19:59:03 -0500

    service should work now

--- app/service/pathfinding_application_service.py
+++ app/service/pathfinding_application_service.py
@@ -4,10 +4,12 @@ from app.pathfinding.pathfinding import PathFinding
 
 def find(obstacles, width, length, robot_position, destination):
     grid = Grid(width, length)
+
+    for obstacle in obstacles:
+        grid.add_obstacle(obstacle)
+
     robot_coordinate = grid.game_board[robot_position.x][robot_position.y]
     destination_coordinate = grid.game_board[destination.x][destination.y]
     pathfinder = PathFinding(grid, robot_coordinate,
                              destination_coordinate)
-    for obstacle in obstacles:
-        pathfinder.add_obstacle(obstacle)
     return pathfinder.find_path()
