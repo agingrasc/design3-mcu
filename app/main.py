6a81695f6d86db9cbff37ba082ef065b6692913b
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2017-02-27 14:16:52 -0500

    retour 0.0.0.0

--- app/main.py
+++ app/main.py
@@ -49,4 +49,4 @@ if __name__ == '__main__':
         app.register_blueprint(ledok.led_ok)
     else:
         print("Bad arguments : manual or automatic")
-    app.run(host='127.0.0.1')
+    app.run(host='0.0.0.0')
