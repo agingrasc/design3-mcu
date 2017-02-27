4a4a407064fc4e52169b3034d182d5aa3389dc92
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2017-02-27 12:23:27 -0500

    Integration gotoposition et regulateur proportionnel en position
    * endpoint goto retourne un code d'erreur si le payload est invalide
    * extraction de constantes pour le regulateur
    * regulateur modifie pour commande en %PWM
    * relinearization de la commande selon commande minimale, la deadzone et commande maximale
    * creation et envoi de la commande Move
    * mise a jour du fichier protocole.py

--- app/api/gotoposition.py
+++ app/api/gotoposition.py
@@ -1,13 +1,27 @@
 from flask import Blueprint, request, make_response, jsonify
 
+from ..mcu.robotcontroller import robot_controller
+from ..mcu.commands import regulator, Move
+
 go_to_position = Blueprint('go-to-position', __name__)
 
 
 @go_to_position.route('/go-to-position', methods=['POST'])
 def go_to_position_():
     print("go-to-position")
-    pos_x = request.json["x"]
-    pos_y = request.json["y"]
+    try:
+        pos_x = request.json["x"]
+        pos_y = request.json["y"]
+        theta = request.json['theta']
+    except KeyError:
+        print("mauvais payload")
+        return make_response(jsonify(), 400)
+
     print(pos_x)
     print(pos_y)
+    print(theta)
+    regulator.set_point(tuple(pos_x, pos_y, 0))
+    x, y, t = regulator.next_speed_command(tuple(0, 0, theta))
+    move = Move(x, y, t)
+    robot_controller.send_command(move)
     return make_response(jsonify({'x': pos_x, 'y': pos_y}), 200)
--- app/mcu/commands.py
+++ app/mcu/commands.py
@@ -8,18 +8,28 @@ from . import protocol
 from .protocol import PencilStatus, Leds
 
 
-PIDConstants = namedtuple("PIDConstatns", 'k_gain i_gain d_gain max_cmd min_cmd')
+PIDConstants = namedtuple("PIDConstatns", 'kp ki kd max_cmd deadzone_cmd min_cmd')
 DEADZONE = 15
-DEFAULT_DELTA_T = 0.030  # 30ms, a modifier
+DEFAULT_DELTA_T = 0.067  # 30ms, a modifier
+MAX_X = 200
+MAX_Y = 100
+
+DEFAULT_KP = 100
+DEFAULT_KI = 0
+DEFAULT_KD = 0
+DEFAULT_MAX_CMD = 80
+DEFAULT_DEADZONE_CMD = 40
+DEFAULT_MIN_CMD = 10
 
 
 class PositionRegulator(object):
     """ Implémente un régulateur PI qui agit avec une rétroaction en position et génère une commande de vitesse."""
 
-    def __init__(self):
+    def __init__(self, kp=DEFAULT_KP, ki=DEFAULT_KI, kd=DEFAULT_KD, max_cmd=DEFAULT_MAX_CMD,
+                 deadzone_cmd=DEFAULT_DEADZONE_CMD, min_cmd=DEFAULT_MIN_CMD):
         self._set_point = 0, 0, 0
         self.accumulator = 0, 0, 0
-        self.constants = PIDConstants(1, 0, 0, 125, 0)
+        self.constants = PIDConstants(kp, ki, kd, max_cmd, deadzone_cmd, min_cmd)
 
     @property
     def set_point(self):
@@ -36,33 +46,37 @@ class PositionRegulator(object):
         dest_x, dest_y, dest_theta = self.set_point
         err_x, err_y, err_theta = dest_x - actual_x, dest_y - actual_y, dest_theta - actual_theta
 
-        # Proportionnel 1:1 (completement inutile)
-        command = err_x, err_y, err_theta
-        saturated_command = self._saturate_command(command)
-        # TODO: ajuster le x et y en fonction de l'orientation
+        cmd_x = err_x / MAX_X * self.constants.kp
+        cmd_y = err_y / MAX_Y * self.constants.kp
+        cmd_x= self._relinearize(cmd_x)
+        cmd_y= self._relinearize(cmd_y)
+        cmd_x, cmd_y = _correct_for_referential_frame(cmd_x, cmd_y, actual_position)
+        command = cmd_x, cmd_y, err_theta
 
         if math.sqrt(err_x**2 + err_y**2) < DEADZONE:
-            saturated_command = 0, 0, 0
-        return saturated_command
+            command = 0, 0, 0
+        return command
 
-    def _saturate_command(self, command):
-        """"
-        S'assure que chaque composante de la commande est dans l'interval [-max_cmd, -min_cmd] or [min_cmd, max_cmd]
-        Args:
-            :command tuple(x, y, t): composantes de la commande
-        Returns:
-            La commande saturée
-        """
-        saturated_command = [0, 0, 0]
-        for idx, cmd_part in enumerate(command):
-            if cmd_part < -self.constants.max_cmd:
-                saturated_command[idx] = -self.constants.max_cmd
-            elif cmd_part > self.constants.max_cmd:
-                saturated_command[idx] = self.constants.max_cmd
-            elif -self.constants.min_cmd < cmd_part < self.constants.min_cmd:
-                # FIXME: il faudra probablement relineariser si la commande n'est pas au minimum
-                saturated_command[idx] = 0
-        return tuple(saturated_command)
+    def _relinearize(self, cmd):
+        """" Force la valeur de cmd dans [deadzone_cmd, max_cmd] ou 0 si dans [-min_cmd, min_cmd]"""
+        if self.constants.min_cmd < cmd < self.constants.deadzone_cmd:
+            return self.constants.deadzone_cmd
+        elif -self.constants.deadzone_cmd < cmd < -self.constants.min_cmd:
+            return -self.constants.deadzone_cmd
+        elif -self.constants.min_cmd <= cmd <= self.constants.min_cmd:
+            return 0
+        else:
+            return cmd
+
+
+def _correct_for_referential_frame(x, y, t):
+
+    cos = math.cos(t)
+    sin = math.sin(t)
+
+    corrected_x = (x * cos - y * sin)
+    corrected_y = (y * cos + x * sin)
+    return corrected_x, corrected_y
 
 
 # regulator static et persistent
